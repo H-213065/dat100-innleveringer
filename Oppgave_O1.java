215352767ddfd4d71b2c58b8fbd6797d179838a9
H-213065 <213065@stud.hvl.no>
2023-09-03 18:06:43 +0200

    Konvertert til sløyfe

--- Oppgave_O1.java
+++ Oppgave_O1.java
@@ -17,27 +17,34 @@ public class Oppgave_O1 {
 			return;
 		}
 		
+		// Mottatt inntastede data.
 		double bruttoLønn = parseInt(tallTxt);
+		// Initialisering.
 		double skatt = 0.0;
 		
 		// Oppdatert fra tabell på sida https://www.skatteetaten.no/satser/trinnskatt/?year=2023#rateShowYear
+		double[] trinnGrenser = {198350,279150,642950,926800,1500000};
+		double[] trinnProsenter = {1.7,4,13.5,16.5,17.5};
+		int antallTrinn = trinnGrenser.length;
+
 		
-		if (bruttoLønn > 198349) {
-			skatt = 1.7;		
-		}
-		if (bruttoLønn > 279149 ) {
-			skatt = 4.0;	
-		}
-		if (bruttoLønn > 642949) {
-			skatt = 13.5;	
-		}
-		if (bruttoLønn > 926799) {
-			skatt = 16.5;	
-		}
-		if (bruttoLønn > 1499999) {
-			skatt = 17.5;	
+		// Går gjennom baklengs beløpsgrenser til vi ev. får treff.
+		for (int i = antallTrinn - 1;i >= 0;i--) {
+			double grense = trinnGrenser[i];
+			double prosent = trinnProsenter[i];
+			
+			if (bruttoLønn >= grense) {
+
+				skatt = prosent;
+				
+				// Riktig trinn ble funnet og i settes til -1 for å stoppe sløyfen.
+				i = -1;
+				
+			}
+			
 		}
 		
+		// Kalkulasjon og utskrift.
 		double resultat = (skatt * bruttoLønn) / 100;
 		showMessageDialog(null,"Skatteprosent er " + skatt + " som i skatt blir:\n" + (int) resultat + ".");
 		
