e32f0359bbcf25ed08afdca7b189e26ff76c23b4
H-213065 <213065@stud.hvl.no>
2023-09-06 08:28:17 +0200

    Lagt inn try-catch

--- Oppgave_O1.java
+++ Oppgave_O1.java
@@ -8,6 +8,21 @@ public class Oppgave_O1 {
 	// Trinnskatt
 
 	public static void main(String[] args) {
+		
+		
+		// INITIALISERING
+		
+		double bruttoLønn = 0.0;
+		double skatt = 0.0;
+		
+		// Oppdatert fra tabell på sida https://www.skatteetaten.no/satser/trinnskatt/?year=2023#rateShowYear
+		final double[] TRINN_GRENSEVERDIER = {198350,279150,642950,926800,1500000};
+		final double[] TRINN_PROSENTVERDIER = {1.7,4,13.5,16.5,17.5};
+		final int ANT_TRINN = TRINN_GRENSEVERDIER.length;
+		
+
+		// KJØRING
+		
 		// Hente inn verdi for n
 		String tallTxt = showInputDialog("Skriv inn verdien for n:");
 		
@@ -17,37 +32,41 @@ public class Oppgave_O1 {
 			return;
 		}
 		
-		// Mottatt inntastede data.
-		double bruttoLønn = parseInt(tallTxt);
-		// Initialisering.
-		double skatt = 0.0;
-		
-		// Oppdatert fra tabell på sida https://www.skatteetaten.no/satser/trinnskatt/?year=2023#rateShowYear
-		double[] trinnGrenser = {198350,279150,642950,926800,1500000};
-		double[] trinnProsenter = {1.7,4,13.5,16.5,17.5};
-		int antallTrinn = trinnGrenser.length;
-
-		
-		// Går gjennom baklengs beløpsgrenser til vi ev. får treff.
-		for (int i = antallTrinn - 1;i >= 0;i--) {
-			double grense = trinnGrenser[i];
-			double prosent = trinnProsenter[i];
+		try {
 			
-			if (bruttoLønn >= grense) {
-
-				skatt = prosent;
+			// Mottatt inntastede data.
+			bruttoLønn = parseInt(tallTxt);
+		
+			// Går NEDOVER i beløpsgrenser til vi ev. får treff.
+			
+			for (int i = ANT_TRINN - 1;i >= 0;i--) {
 				
-				// Riktig trinn ble funnet og i settes til -1 for å stoppe sløyfen.
-				i = -1;
+				if (bruttoLønn >= TRINN_GRENSEVERDIER[i]) {
+	
+					// Riktig trinn ble funnet
+					skatt = TRINN_PROSENTVERDIER[i];
+					
+					// Tellevariabelen i settes til verdi utenfor spenn, for å stoppe sløyfen.
+					i = -1;
+					
+				}
 				
 			}
 			
+			// Endelig kalkulasjon.
+			double resultat = (skatt * bruttoLønn) / 100;
+			// Utskrift.
+			showMessageDialog(null,"Trinnskatt blir på " + skatt + "% som vil utgjøre:\n" + (int) resultat + " kr.");
+		
+		}catch(Exception e) {
+			
+			// Inntastede data sannsynligvis bogus.
+			System.out.println("Utregning ikke mulig: " + e);
+			showMessageDialog(null,
+			    "Kunne ikke regne ut. Du må starte appen på nytt.",
+			    "Utilstrekkelige data",
+			    WARNING_MESSAGE);
 		}
-		
-		// Kalkulasjon og utskrift.
-		double resultat = (skatt * bruttoLønn) / 100;
-		showMessageDialog(null,"Skatteprosent er " + skatt + " som i skatt blir:\n" + (int) resultat + ".");
-		
 	}
 
 }
